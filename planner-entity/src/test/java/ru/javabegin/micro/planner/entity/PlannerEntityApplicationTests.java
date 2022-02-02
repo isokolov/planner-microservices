42695d980e08deb8cec51bc82590dbae0fe96fa0
isokolov <illya.sokolov82@gmail.com>
2022-02-02 16:51:25 +0200

    planner entity service with old project entities

---
+++ planner-entity/src/test/java/ru/javabegin/micro/planner/entity/PlannerEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.planner.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
