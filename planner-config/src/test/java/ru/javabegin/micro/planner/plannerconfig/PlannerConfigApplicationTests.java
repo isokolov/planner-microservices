23d66654796fe280e48854cfb9728c97c905f907
isokolov <illya.sokolov82@gmail.com>
2022-02-02 15:28:12 +0200

    Planner microservices initial commit

---
+++ planner-config/src/test/java/ru/javabegin/micro/planner/plannerconfig/PlannerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.planner.plannerconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ planner-server/src/test/java/ru/javabegin/micro/planner/plannerserver/PlannerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.planner.plannerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
