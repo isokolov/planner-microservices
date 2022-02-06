ccf20a3600d4d27d92175ca43267232ae413adb4
isokolov <illya.sokolov82@gmail.com>
2022-02-06 22:25:20 +0200

    adding planner-users service & aop

---
+++ planner-users/src/main/java/ru/javabegin/micro/planner/users/PlannerUsersApplication.java
@@ -0,0 +1,15 @@
+package ru.javabegin.micro.planner.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PlannerUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerUsersApplication.class, args);
+    }
+
+}
